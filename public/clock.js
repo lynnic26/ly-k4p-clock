d6a75e045cb27320ee89fb00890a15b553cfd2f9
GuanNing <guanning@patsnap.com>
2017-05-14 00:59:57 +0800

    add define function for clock.js

--- public/clock.js
+++ public/clock.js
@@ -1,20 +1,23 @@
-require('plugins/ly_k4p_clock/clock.css');
-require('plugins/ly_k4p_clock/clockController');
-function ClockProvider (Private) {
-    var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
-    return new TemplateVisType({
-        name: 'lyClock', // the internal id of the visualization
-        title: 'Clock', // the name shown in the visualize list
-        icon: 'fa-clock-o', // the class of the font awesome icon for this
-        description: 'Add a digital clock to your dashboard.', // description shown to the users
-        requiresSearch: false, // Cannot be linked to a search
-        template: require('plugins/ly_k4p_clock/clock.html'), // load the template of the visualization
-        params: {
-            editor: require('plugins/ly_k4p_clock/clock-editor.html'),
-            defaults: {
-                format: 'HH:mm:ss'
+define(function (require) {
+    require('plugins/ly_k4p_clock/clock.css');
+    require('plugins/ly_k4p_clock/clockController');
+    function ClockProvider (Private) {
+        var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
+        return new TemplateVisType({
+            name: 'lyClock', // the internal id of the visualization
+            title: 'Clock', // the name shown in the visualize list
+            icon: 'fa-clock-o', // the class of the font awesome icon for this
+            description: 'Add a digital clock to your dashboard.', // description shown to the users
+            requiresSearch: false, // Cannot be linked to a search
+            template: require('plugins/ly_k4p_clock/clock.html'), // load the template of the visualization
+            params: {
+                editor: require('plugins/ly_k4p_clock/clock-editor.html'),
+                defaults: {
+                    format: 'HH:mm:ss'
+                }
             }
-        }
-    });
-}
-require('ui/registry/vis_types').register(ClockProvider);
+        });
+    }
+    require('ui/registry/vis_types').register(ClockProvider);
+    return ClockProvider;
+});
